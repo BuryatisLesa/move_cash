4dc7879f2ce9ee3742b3a914bc46cf564cea7d80
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-25 04:45:29 +0800

    feat: добавлена валидация данных, добавлена форма в form_class Для MoveCash, так добавлена метод view для регистрации пользователей

--- move_cash/app_cash/views.py
+++ move_cash/app_cash/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.urls import reverse_lazy
 from app_cash.models import MoveCash
+from app_cash.forms import MoveCashForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import PermissionDenied
 
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 class MoveCashList(ListView):
     model = MoveCash
@@ -17,20 +31,43 @@ class MoveCashDetail(DetailView):
     template_name = "move_cash/move-cash-detail.html"
     context_object_name = "movecash"
 
-class MoveCashCreate(CreateView):
+class MoveCashCreate(LoginRequiredMixin, CreateView):
     model = MoveCash
     template_name = "move_cash/move-cash-create.html"
-    form_class = None
+    form_class = MoveCashForm
+
+    def form_valid(self, form):
+        # проверка авторизации
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
     success_url = reverse_lazy("movecash_list")
 
-class MoveCashUpdate(UpdateView):
+class MoveCashUpdate(LoginRequiredMixin, UpdateView):
     model = MoveCash
     template_name = "move_cash/move-cash-create.html"
-    form_class = None
+    form_class = MoveCashForm
+
+    def dispatch(self, request, *args, **kwargs):
+        # проверка прав пользователя
+        movecash = self.get_object()
+        if movecash.user != request.user:
+            raise PermissionDenied("У вас нет прав для редактирование данной операции")
+        return super().dispatch(request, *args, **kwargs)
+
     success_url = reverse_lazy("movecash_list")
 
 
-class MoveCashDelete(DeleteView):
+class MoveCashDelete(LoginRequiredMixin, DeleteView):
     model = MoveCash
     template_name = "move_cash/move-cash-delete.html"
+
+
+    def dispatch(self, request, *args, **kwargs):
+        # проверка прав пользователя
+        movecash = self.get_object()
+        if movecash.user != request.user:
+            raise PermissionDenied("У вас нет прав для удаление данной операции")
+        return super().dispatch(request, *args, **kwargs)
+    
     success_url = reverse_lazy("movecash_list")
