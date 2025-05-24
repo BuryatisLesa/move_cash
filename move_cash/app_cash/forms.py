d03f49e47580fde5ddb91ae56ffe8224bc18fb9d
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-25 03:55:44 +0800

    feat: создан файл forms.py для форм, а также расписана форма MoveCashForm

---
+++ move_cash/app_cash/forms.py
@@ -0,0 +1,28 @@
+from app_cash.models import MoveCash
+from django import forms
+
+class MoveCashForm(forms.ModelForm):
+    class Meta:
+        model = MoveCash
+        fields = "__all__"
+        widgets = {
+            "created_at": forms.DateTimeInput(
+                attrs={'type': 'datetime-local', 'class': 'form-control'},
+                format='%Y-%m-%dT%H:%M'
+            ),
+            "status": forms.Select(attrs={"class": "form-control", "placeholder": "Выберите статус операции"}),
+            "typeoperation": forms.Select(attrs={"class": "form-control", "placeholder": "Выберите тип операции"}),
+            "category": forms.Select(attrs={"class": "form-control", "placeholder": "Категория операции"}),
+            "subcategory": forms.Select(attrs={"class": "form-control", "placeholder": "Подкатегория операции"}),
+            "sum_operation": forms.NumberInput(attrs={"min": 0, "step": 0.01, "class": "form-control", "placeholder": "Введите сумму"}),
+            "comment": forms.TextInput(attrs={"class": "form-control", "placeholder": "Введите комментарий"})
+        }
+        labels = {
+            "created_at": "Дата",
+            "status": "Статус",
+            "typeoperation": "Операция",
+            "category": "Категория",
+            "subcategory": "Подкатегория",
+            "sum_operation": "Сумма",
+            "comment": "Комментарий"
+        }
